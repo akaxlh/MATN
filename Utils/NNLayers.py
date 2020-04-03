2c381d272fdf49befbe624c4f6f8a0bd6ad3f0cc
anonymous0402 <63047255+anonymous0402@users.noreply.github.com>
2020-04-03 21:53:59 +0800

    Upload Utils files

---
+++ Utils/NNLayers.py
@@ -0,0 +1,179 @@
+import tensorflow as tf
+from tensorflow.contrib.layers import xavier_initializer
+import numpy as np
+
+paramId = 0
+biasDefault = False
+params = {}
+regParams = {}
+ita = 0.2
+leaky = 0.01
+
+def getParamId():
+	global paramId
+	paramId += 1
+	return paramId
+
+def setIta(ITA):
+	ita = ITA
+
+def setBiasDefault(val):
+	global biasDefault
+	biasDefault = val
+
+def getParam(name):
+	return params[name]
+
+def addReg(name, param):
+	global regParams
+	if name not in regParams:
+		regParams[name] = param
+	# else:
+	# 	print('ERROR: Parameter already exists')
+
+def addParam(name, param):
+	global params
+	if name not in params:
+		params[name] = param
+
+def defineParam(name, shape, dtype=tf.float32, reg=False,
+	initializer='xavier', trainable=True):
+	global params
+	global regParams
+	if initializer == 'xavier':
+		ret = tf.get_variable(name=name, dtype=dtype, shape=shape,
+			initializer=xavier_initializer(dtype=tf.float32),
+			trainable=trainable)
+	elif initializer == 'trunc_normal':
+		ret = tf.get_variable(name=name, initializer=tf.random.truncated_normal(shape=[int(shape[0]), shape[1]], mean=0.0, stddev=0.03, dtype=dtype))
+	elif initializer == 'zeros':
+		ret = tf.get_variable(name=name, dtype=dtype,
+			initializer=tf.zeros(shape=shape, dtype=tf.float32),
+			trainable=trainable)
+	elif initializer == 'ones':
+		ret = tf.get_variable(name=name, dtype=dtype, initializer=tf.ones(shape=shape, dtype=tf.float32), trainable=trainable)
+	elif not isinstance(initializer, str):
+		ret = tf.get_variable(name=name, dtype=dtype,
+			initializer=initializer, trainable=trainable)
+	else:
+		print('ERROR: Unrecognized initializer')
+		exit()
+	params[name] = ret
+	if reg:
+		regParams[name] = ret
+	return ret
+
+def getOrDefineParam(name, shape, dtype=tf.float32, reg=False,
+	initializer='xavier', trainable=True):
+	global params
+	global regParams
+	if name in params:
+		if reg and name not in regParams:
+			regParams[name] = params[name]
+		return params[name]
+	return defineParam(name, shape, dtype, reg, initializer, trainable)
+
+def BN(inp, name=None):
+	global ita
+	dim = inp.get_shape()[1]
+	name = 'defaultParamName%d'%getParamId()
+	scale = tf.Variable(tf.ones([dim]))
+	shift = tf.Variable(tf.zeros([dim]))
+	fcMean, fcVar = tf.nn.moments(inp, axes=[0])
+	ema = tf.train.ExponentialMovingAverage(decay=0.5)
+	emaApplyOp = ema.apply([fcMean, fcVar])
+	with tf.control_dependencies([emaApplyOp]):
+		mean = tf.identity(fcMean)
+		var = tf.identity(fcVar)
+	ret = tf.nn.batch_normalization(inp, mean, var, shift,
+		scale, 1e-8)
+	return ret
+
+def FC(inp, outDim, name=None, useBias=False, activation=None,
+	reg=False, useBN=False, dropout=None, initializer='xavier', noDrop=False):
+	global params
+	global regParams
+	global leaky
+	# useBias = biasDefault
+	# if not noDrop:
+	# 	inp = tf.nn.dropout(inp, rate=0.001)
+	inDim = inp.get_shape()[1]
+	temName = name if name!=None else 'defaultParamName%d'%getParamId()
+	W = getOrDefineParam(temName, [inDim, outDim], reg=reg, initializer=initializer)
+	if dropout != None:
+		ret = tf.nn.dropout(inp, rate=dropout) @ W
+	else:
+		ret = inp @ W
+	if useBias:
+		temBiasName = temName + 'Bias'
+		bias = getOrDefineParam(temBiasName, outDim, reg=False, initializer='zeros')
+		ret = ret + bias
+	if useBN:
+		ret = BN(ret)
+	if activation != None:
+		ret = Activate(ret, activation)
+	return ret
+
+def Bias(data, name=None, reg=False):
+	inDim = data.get_shape()[-1]
+	temName = name if name!=None else 'defaultParamName%d'%getParamId()
+	temBiasName = temName + 'Bias'
+	bias = getOrDefineParam(temBiasName, inDim, reg=False, initializer='zeros')
+	if reg:
+		regParams[temBiasName] = bias
+	return data + bias
+
+def ActivateHelp(data, method):
+	if method == 'relu':
+		ret = tf.nn.relu(data)
+	elif method == 'sigmoid':
+		ret = tf.nn.sigmoid(data)
+	elif method == 'tanh':
+		ret = tf.nn.tanh(data)
+	elif method == 'softmax':
+		ret = tf.nn.softmax(data, axis=-1)
+	elif method == 'leakyRelu':
+		ret = tf.maximum(leaky*data, data)
+	elif method == 'twoWayLeakyRelu':
+		temMask = tf.to_float(tf.greater(data, 1.0))
+		ret = temMask * (1 + leaky * (data - 1)) + (1 - temMask) * tf.maximum(leaky * data, data)
+	elif method == '-1relu':
+		ret = tf.maximum(-1.0, data)
+	elif method == 'relu6':
+		ret = tf.maximum(0.0, tf.minimum(6.0, data))
+	else:
+		raise Exception('Error Activation Function')
+	return ret
+
+def Activate(data, method, useBN=False):
+	global leaky
+	if useBN:
+		ret = BN(data)
+	else:
+		ret = data
+	ret = ActivateHelp(ret, method)
+	return ret
+
+def Regularize(names=None, method='L2'):
+	ret = 0
+	if method == 'L1':
+		if names != None:
+			for name in names:
+				ret += tf.reduce_sum(tf.abs(getParam(name)))
+		else:
+			for name in regParams:
+				ret += tf.reduce_sum(tf.abs(regParams[name]))
+	elif method == 'L2':
+		if names != None:
+			for name in names:
+				ret += tf.reduce_sum(tf.square(getParam(name)))
+		else:
+			for name in regParams:
+				ret += tf.reduce_sum(tf.square(regParams[name]))
+	return ret
+
+def Dropout(data, rate):
+	if rate == None:
+		return data
+	else:
+		return tf.nn.dropout(data, rate=rate)
